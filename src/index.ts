6e30ca452b5f61a2852956357be939c1f60fbee9
Rodrigo Gibran Felix Leon <yoyiyomeco@hotmail.com>
2024-12-09 22:09:23 -0800

    fast fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-const QRCode = require('qrcode');
+const QRCode = require("qrcode");
 
-const contenido = '';
+const contenido = "https://www.facebook.com/61568505023231";
 
-QRCode.toFile('./out-qr/qrcode.png', contenido, function (err : any) {
-    if (err) throw err
-    console.log('The Qr code saved in output folder!');
-});
+QRCode.toFile(
+	"./out-qr/qrcode.png",
+	contenido,
+	{
+		color: {
+			dark: "#FFFFFF", // QR code color
+			light: "#000000", // Background color
+		},
+		width: 1000, // Increase the size of the QR code
+		version: 5, // Increase the version of the QR code
+	},
+	(err: Error) => {
+		if (err) throw err;
+		console.log("The Qr code saved in output folder!");
+	},
+);
